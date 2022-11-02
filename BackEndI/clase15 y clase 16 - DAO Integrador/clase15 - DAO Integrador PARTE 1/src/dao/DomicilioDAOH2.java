c1dae3e1f0738114623cabd0f0b3a9b6a08df955
JorgSierra <jorge.sierrar@gmail.com>
2022-11-02 10:11:21 -0500

    actualizacion de contenido

---
+++ BackEndI/clase15 y clase 16 - DAO Integrador/clase15 - DAO Integrador PARTE 1/src/dao/DomicilioDAOH2.java
@@ -0,0 +1,32 @@
+package dao;
+
+import model.Domicilio;
+
+import java.util.List;
+
+public class DomicilioDAOH2 implements IDao<Domicilio>{
+    @Override
+    public Domicilio guardar(Domicilio domicilio) {
+        return null;
+    }
+
+    @Override
+    public Domicilio buscar(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void actualizar(Domicilio domicilio) {
+
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+
+    }
+
+    @Override
+    public List<Domicilio> buscarTodo() {
+        return null;
+    }
+}

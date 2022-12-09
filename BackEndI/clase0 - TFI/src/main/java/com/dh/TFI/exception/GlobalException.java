a7f9b1859051985c7950b4ec7ef9b0fd991e1c5c
JorgSierra <jorge.sierrar@gmail.com>
2022-12-09 14:16:32 -0800

    Exeption logger added

--- BackEndI/clase0 - TFI/src/main/java/com/dh/TFI/exception/GlobalException.java
+++ BackEndI/clase0 - TFI/src/main/java/com/dh/TFI/exception/GlobalException.java
@@ -1,5 +1,6 @@
 package com.dh.TFI.exception;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,12 +8,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalException{
+    private final static Logger LOGGER = Logger.getLogger(GlobalException.class);
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<String> handleResourceNotFound (ResourceNotFoundException e){
+        LOGGER.error("NO ENCONTRADO mensaje: " + e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
     @ExceptionHandler({BadRequestException.class})
     public ResponseEntity<String> handleBadRequest (BadRequestException e){
+        LOGGER.error("BAD REQUEST mensaje: " + e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
